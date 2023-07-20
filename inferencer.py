278e340b8d0a96c205b6775c9c22b0eeb296f1ee
Liu Jinyao <liujinyao@pjlab.com>
2023-07-20 11:00:19 +0800

    add

---
+++ inferencer.py
@@ -0,0 +1,33 @@
+from typing import List, Any
+
+from tokenization_internlm import InternLMTokenizer
+from modeling_internlm import InternLMForCausalLM
+from configuration_internlm import InternLMConfig
+
+
+class Inferencer:
+
+    def __init__(self) -> None:
+        self.tokenizer = InternLMTokenizer("tokenizer.model", '<unk', '<s>', '</s>', '</s>')
+
+        # tokens = tokenizer.tokenize("How are you?")
+        # print(tokens)
+
+        # ids = tokenizer.convert_tokens_to_ids(tokens)
+        # print(ids)
+
+        self.model = InternLMForCausalLM(InternLMConfig())
+        self.model.eval()
+        # response, history = model.chat(tokenizer, "hello", history=[])
+
+
+    def __call__(self, *args: Any, **kwds: Any) -> List:
+        return self.model.chat(self.tokenizer, "hello", history=[])
+
+
+inferencer = Inferencer()
+
+
+
+
+
