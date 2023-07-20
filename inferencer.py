4381819277b8ef2aef9887e1c4d5069d37cc980d
liujinyao <shadaniljy@126.com>
2023-07-20 16:37:59 +0800

    Update inferencer.py

--- inferencer.py
+++ inferencer.py
@@ -8,7 +8,7 @@ from configuration_internlm import InternLMConfig
 class Inferencer:
 
     def __init__(self) -> None:
-        self.tokenizer = InternLMTokenizer("tokenizer.model", '<unk', '<s>', '</s>', '</s>')
+        #self.tokenizer = InternLMTokenizer("tokenizer.model", '<unk', '<s>', '</s>', '</s>')
 
         # tokens = tokenizer.tokenize("How are you?")
         # print(tokens)
@@ -16,13 +16,15 @@ class Inferencer:
         # ids = tokenizer.convert_tokens_to_ids(tokens)
         # print(ids)
 
-        self.model = InternLMForCausalLM(InternLMConfig())
-        self.model.eval()
+        #self.model = InternLMForCausalLM(InternLMConfig())
+        #self.model.eval()
         # response, history = model.chat(tokenizer, "hello", history=[])
+        pass
 
 
     def __call__(self, *args: Any, **kwds: Any) -> List:
-        return self.model.chat(self.tokenizer, "hello", history=[])
+        #return self.model.chat(self.tokenizer, "hello", history=[])
+        return "hello"
 
 
 inferencer = Inferencer()
